efed097d1f567ff3d6972124adf1417a218ddf15
Leonardo Gurgel <lgurgel@id.uff.br>
2021-07-08 20:05:44 -0300

    generate coverage

--- generate-coverage.sh
+++ generate-coverage.sh
@@ -1,4 +1,4 @@
 export RUSTC_BOOTSTRAP=1
 export RUSTFLAGS="-Zinstrument-coverage"
-export LLVM_PROFILE_FILE="lgurgel-%p-%m.profraw"
+export LLVM_PROFILE_FILE="coverage/lgurgel-%p-%m.profraw"
 cargo build && cargo test && grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./coverage/
